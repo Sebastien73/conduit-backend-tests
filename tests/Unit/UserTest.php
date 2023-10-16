5a594214a4b68ca2f8af6804f7704fc349aa930b
Sébastien Jacquelin <sebastien.jacquelin@le-campus-numerique.fr>
2023-10-16 15:57:31 +0200

    First step iteration 1

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -4,15 +4,19 @@ namespace Tests\Unit;
 
 use App\Models\Article;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 class UserTest extends TestCase
 {
     use RefreshDatabase;
+
     protected $seed = true;
+
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
-        parent::__construct($name,$data,$dataName);
+        parent::__construct($name, $data, $dataName);
     }
 
     /**
@@ -24,21 +28,72 @@ class UserTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
     public function test_getRouteKeyName()
     {
-        $this->assertEquals('username',User::make()->getRouteKeyName());
+        $this->assertEquals('username', User::make()->getRouteKeyName());
     }
 
     public function test_articles()
     {
         $rose = User::find(1);
-        $this->assertEquals($rose->id, $rose->articles->count());
+        // Méthod pluck -> prends tout les éléments d'un tableau et garde seulement les attributs que l'on aura demandé.
 
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $rose->articles);
+        $this->assertEquals(
+            Article::where('user_id', $rose->id)->get()->pluck(['id', 'title', 'slug', 'description']),
+            $rose->articles->pluck(['id', 'title', 'slug', 'description'])
+        );
     }
-    //public function test_favouriteArticles(){}
-    //public function test_followers(){}
-    //public function test_following(){}
-    //public function test_doesUserFollowAnotherUser(){}
+
+    public function test_favouriteArticles()
+    {
+        //Where has = filtre au niveau des relations du modèle (côté bdd)
+        // Filter on model Article with relationship user
+        // In builder query, search with where clause (column id of user table, id of user specific
+        // Don't forgot ->get() to keep result of query
+        // Don't forgot ->pluck after ->get() for take all elements of array and keep only attribute specific
+        // Here, test all articles where has relationship with user musonda
+        $musonda = User::find(2);
+
+        $this->assertEquals(
+            Article::whereHas('users', function (Builder $query) use($musonda){
+                $query->where('id', $musonda->id);
+            })->get()->pluck(['id', 'title', 'slug', 'description']),
+            $musonda->favoritedArticles->pluck(['id', 'title', 'slug', 'description'])
+        );
+    }
+
+    public function test_followers()
+    {
+        $rose = User::find(1);
+
+        $this->assertEquals(
+            User::whereHas('following', function (Builder $query) use($rose){
+                $query->where('following_id', $rose->id);
+            })->get()->pluck(['username','email','bio']),
+            $rose->followers->pluck(['username','email','bio'])
+        );
+    }
+
+    public function test_following()
+    {
+        $rose = User::find(1);
+
+        $this->assertEquals(
+            User::whereHas('followers', function (Builder $query) use($rose){
+                $query->where('follower_id', $rose->id);
+            })->get()->pluck(['username','email','bio']),
+            $rose->followers->pluck(['username','email','bio'])
+        );
+    }
+
+    public function test_doesUserFollowAnotherUser()
+    {
+        $rose = User::find(1);
+        $musonda = User::find(2);
+
+        $this->assertTrue($rose->doesUserFollowAnotherUser($rose->id, $musonda->id));
+        $this->assertFalse($rose->doesUserFollowAnotherUser($rose->id,4));
+    }
 
 }
